7adbc9900627124a6a7383ae78fb1332da54fe88
Jin <hjd007wwkd@hotmail.com>
2018-06-30 02:09:43 +0000

    connected with database

--- main/src/components/carousel.js
+++ main/src/components/carousel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Room from './room';
 
-const Carousel = () => {
+const Carousel = (props) => {
   
   const renderCarouselColumn = (n) => {
     const start = n * 3;
@@ -9,10 +9,12 @@ const Carousel = () => {
     const isActive = !n;
     const carouselColumnArray = [];
     
-    for(let i = start; i < end; i ++) {
-      carouselColumnArray.push(
-        <Room data={featuredArray[i]} inCarousel={true} />
-      )
+    if(props.feature.length !== 0) {
+      for(let i = start; i < end; i ++) {
+        carouselColumnArray.push(
+          <Room data={props.feature[i]} inCarousel={true} />
+        )
+      }
     }
     
     return (
@@ -44,104 +46,3 @@ const Carousel = () => {
 }
 
 export default Carousel;
-
-
-
-
-
-
-
-
-
-const featuredArray = [
-  {
-    roomID: 1,
-    roomName: 'Sports Central',
-    owner: {
-      username: 'Bob3452',
-      avatar: 'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png'
-    },
-    roomImage: 'https://i.ytimg.com/vi/YBi6-UoXxZw/maxresdefault.jpg',
-    usersOnline: 10
-  },
-  {
-    roomID: 2,
-    roomName: 'Politics Now',
-    owner: {
-      username: 'Karl4563452',
-      avatar: 'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png'
-    },
-    roomImage: 'https://static.makeuseof.com/wp-content/uploads/2017/08/twitch-change-video-gaming-670x335.jpg',
-    usersOnline: 134
-  },
-  {
-    roomID: 3,
-    roomName: 'eSports eSports',
-    owner: {
-      username: 'Sarah45345',
-      avatar: 'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png'
-    },
-    roomImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRczL8wU1EYEbH9eFpSVoM7UwKeQDAsNtqEhL0S155TUcN_bdUO',
-    usersOnline: 304
-  },
-  {
-    roomID: 4,
-    roomName: 'FoodCentral',
-    owner: {
-      username: 'Jrodan111',
-      avatar: 'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png'
-    },
-    roomImage: 'https://ptzoptics.com/wp-content/uploads/2017/10/live-streaming-cameras-on-gary.jpg',
-    usersOnline: 3
-  },
-  {
-    roomID: 5,
-    roomName: 'Gordon Ramsay',
-    owner: {
-      username: 'Bob3452',
-      avatar: 'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png'
-    },
-    roomImage: 'https://i.ytimg.com/vi/YBi6-UoXxZw/maxresdefault.jpg',
-    usersOnline: 10
-  },
-  {
-    roomID: 6,
-    roomName: 'The Young Turks',
-    owner: {
-      username: 'Karl4563452',
-      avatar: 'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png'
-    },
-    roomImage: 'https://static.makeuseof.com/wp-content/uploads/2017/08/twitch-change-video-gaming-670x335.jpg',
-    usersOnline: 134
-  },
-  {
-    roomID: 7,
-    roomName: 'Russia Today',
-    owner: {
-      username: 'Sarah45345',
-      avatar: 'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png'
-    },
-    roomImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRczL8wU1EYEbH9eFpSVoM7UwKeQDAsNtqEhL0S155TUcN_bdUO',
-    usersOnline: 304
-  },
-  {
-    roomID: 8,
-    roomName: 'Hot Wings',
-    owner: {
-      username: 'Jrodan111',
-      avatar: 'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png'
-    },
-    roomImage: 'https://ptzoptics.com/wp-content/uploads/2017/10/live-streaming-cameras-on-gary.jpg',
-    usersOnline: 3
-  },
-  {
-    roomID: 9,
-    roomName: 'Starcraft Player',
-    owner: {
-      username: 'Bob3452',
-      avatar: 'https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/68.png'
-    },
-    roomImage: 'https://i.ytimg.com/vi/YBi6-UoXxZw/maxresdefault.jpg',
-    usersOnline: 10
-  }
-];
