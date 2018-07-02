c177451db02f72bbf89025ef36674b811a406eac
Jin <hjd007wwkd@hotmail.com>
2018-07-02 05:46:24 +0000

    send data to each room

--- server/migrations/20180623030522_table_init.js
+++ server/migrations/20180623030522_table_init.js
@@ -7,35 +7,27 @@ exports.up = function(knex, Promise) {
        table.string('password');
        table.string('avatar');
      }).then(function() {
-  return knex.schema.createTable('topics', function(table) {
-      table.increments('id');
-      table.string('name');
-    }).then(function() {
-  return knex.schema.createTable('subtopics', function(table) {
-      table.increments('id');
-      table.string('name');
-      table.integer('topic_id').notNullable();
-      table.foreign('topic_id').references('id').inTable('topics');
-    }).then(function() {
   return knex.schema.createTable('rooms', function(table) {
       table.increments('id');
-      table.string('name');
+      table.string('title');
       table.string('image');
+      table.string('url');
+      table.string('site');
+      table.timestamp('date');
+      table.specificType('tags', 'text[]');
+      table.text('contenthtml');
+      table.text('contenttext');
       table.integer('user_id').notNullable();
       table.foreign('user_id').references('id').inTable('users');
-      table.integer('subtopic_id').notNullable();
-      table.foreign('subtopic_id').references('id').inTable('subtopics');
     }).then(function() {
   return knex.schema.createTable('messages', function(table) {
       table.increments('id');
-      table.string('content');
+      table.text('content');
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.integer('user_id').notNullable();
       table.foreign('user_id').references('id').inTable('users');
       table.integer('room_id').notNullable();
       table.foreign('room_id').references('id').inTable('rooms');
-    })
-    })
     })
     })
   })
@@ -44,11 +36,7 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable('messages').then(function () {
     return knex.schema.dropTable('rooms').then(function () {
-      return knex.schema.dropTable('subtopics').then(function () {
-        return knex.schema.dropTable('topics').then(function () {
-          return knex.schema.dropTable('users')
-        })
-      })
+      return knex.schema.dropTable('users')
     })
   });
 };
